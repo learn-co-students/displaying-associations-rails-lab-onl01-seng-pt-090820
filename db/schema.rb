bf2d441c931920b974248ed35669b5e0d36019f8
CelinaJackson <celinanjackson@gmail.com>
2021-01-23 15:58:15 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913134502) do
+ActiveRecord::Schema.define(version: 20210123201643) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "song_id"
+  end
+
+  create_table "artists_songs", id: false, force: :cascade do |t|
+    t.integer "artist_id", null: false
+    t.integer "song_id",   null: false
   end
 
   create_table "songs", force: :cascade do |t|
