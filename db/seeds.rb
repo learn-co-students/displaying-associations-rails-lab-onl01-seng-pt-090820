ac06d3dddbb46e864d88bbaa359c77a2f537703f
Erick Castille <ecastille924@gmail.com>
2021-01-25 20:14:00 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+weezer = Artist.create!(name: "Weezer")
+weezer.songs.create!(title: "Say it Ain't So")
+weezer.songs.create!(title: "Hash Pipe")
+weezer.songs.create!(title: "Keep Fishin")
+
+killers = Artist.create!(name: "The Killers")
+killers.songs.create!(title: "Imploding the Mirage")
+killers.songs.create!(title: "Mr. Brightside")
+killers.songs.create!(title: "Sam's Town")
+
+
